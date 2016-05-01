f287cc9c498a4cba24c8a1d793a1d4094e68b458
giancarloavalle <s233534@studenti.polito.it>
2016-05-01 12:17:59 +0200

    Creazione pagina Info e varie Deliverable

---
+++ Info.js
@@ -0,0 +1,14 @@
+/**
+ * Created by giancarloavalle on 01/05/16.
+ */
+function openD1() {
+    window.open("Deliverable%201/Deliverable%201.html");
+}
+
+function openD2() {
+    window.open("Deliverable%202/Deliverable%202.html");
+}
+
+function openD3() {
+    window.open("Deliverable%203/Deliverable%203.html");
+}
