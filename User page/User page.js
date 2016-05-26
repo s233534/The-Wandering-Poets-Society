17d26b3d3085648dd9988bef8e40cc3be9b5082d
giancarloavalle <s233534@studenti.polito.it>
2016-05-26 22:09:41 +0200

    Prova rating, da migliorare

--- User page/User page.js
+++ User page/User page.js
@@ -6,3 +6,11 @@ function openTextEditor() {
     window.open("../Create text/Text Editor.html");
 }
 
+function check(val) {
+    localStorage.setItem("numStars", val);
+}
+function checkStars() {
+    st = localStorage.getItem("numStars");
+    num = "star-" + st;
+    document.getElementById(num).checked=true;
+}
